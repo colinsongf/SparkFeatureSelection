e9ba262b0ac815a8021e6892ac4a1dc7cb2b5200
Sergio Ramirez <srg00017@gmail.com>
2015-04-27 10:13:07 +0200

    Removed problem with changing the range

--- src/main/scala/org/ugr/sci2s/mllib/test/KeelParser.scala
+++ src/main/scala/org/ugr/sci2s/mllib/test/KeelParser.scala
@@ -45,10 +45,11 @@ object KeelParser {
 		require(tokens.length == conv.length)
 		
 		val arr = (conv, tokens).zipped.map{(c, elem) => 
-            c.get("min") match {
+      c.getOrElse(elem, elem.toDouble)
+            /*c.get("min") match {
               case Some(min) => elem.toDouble - min
               case None => c.getOrElse(elem, elem.toDouble)
-            }           
+            } */          
         }
         
 		val features = arr.slice(0, arr.length - 1)
